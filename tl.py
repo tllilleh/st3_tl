17598077f9692d1f418a302659b5dc23525d50cc
Trent Lillehaugen <tllilleh@gmail.com>
2018-12-12 16:59:31 -0800

    added command to close all other windows

--- tl.py
+++ tl.py
@@ -69,3 +69,10 @@ class tlInsertCommentCommand(sublime_plugin.TextCommand):
 			self.insert_c_comment(edit)
 		elif lang == "python":
 			self.insert_python_comment(edit)
+
+
+#class CloseOthersCommand(sublime_plugin.TextCommand):
+#    def run(self, edit):
+#        window = self.view.window()
+#        group_index, view_index = window.get_view_index(self.view)
+#        window.run_command("close_others_by_index", { "group": group_index, "index": view_index})
